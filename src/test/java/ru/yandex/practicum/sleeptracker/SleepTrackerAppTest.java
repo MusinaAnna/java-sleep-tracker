fdccb3d8e21206a1ddbe1125afafc168a64456f9
Anna <mussina1anna@yandex.ru>
2026-02-01 14:10:17 +0300

    fix: исправление имени класса в SleepTrackerAppTest.java

--- src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SleepingSessionCounterTest {
+class SleepTrackerAppTest  {
 
     @Test
     void testApplyWithEmptyList() {
